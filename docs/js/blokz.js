54e1dff1a9c0e1fe14caeea2d8ef8ac1b5e55297
sn0n <are.zee.dee@gmail.com>
2020-05-15 01:24:27 -0700

    updated non profile user information display, bug fixes

--- docs/js/blokz.js
+++ docs/js/blokz.js
@@ -1,9 +1,11 @@
+// TODO: This is part of the 'age' code, clean up later
 const year = new Date();
 const now = new Date().toISOString().split('.')[0];
 
-hive.api.setOptions({ url: 'https://anyx.io' });
+// Set API node
+//hive.api.setOptions({ url: 'https://anyx.io' });
 
-// this checks for url variables like ?steem=sn0n
+// this checks for url variables like ?hive=sn0n
 function getQueryVariable(variable) {
   var query = window.location.search.substring(1);
   var vars = query.split("&");
@@ -13,157 +15,46 @@ function getQueryVariable(variable) {
   }
   return (false);
 }
-
-// 
+// set user to localstorage user
 if (localStorage.getItem("hive") !== null) {
   user = localStorage.getItem("hive");
   console.log(typeof user)
 } else {
   console.log("user does not exist! or something went wrong");
-
 }
-
+// fallback for old steem links
 if (getQueryVariable("steem") !== false) {
   user = getQueryVariable("steem");
+  localStorage.setItem("hive", user);
   console.log(user + " connected");
 }
-
+// set user to hive name in url
 if (getQueryVariable("hive") !== false) {
   user = getQueryVariable("hive");
-
   localStorage.setItem("hive", user);
   console.log(user + " connected");
 }
 
 
 
-// search username on lookup
-function steemagentUp() {
-  console.log("TRIGGERED!!!");
-  let steemagent = document.getElementById("steemagent").value;
-  console.log(steemagent);
-  hive.api.getDiscussionsByAuthorBeforeDate(steemagent, 'blokzprofile', now, 1, (err, result) => {
-
-    // populate data
-    if (result) {
-      console.log("results are in:");
-      console.log(result);
-      var blokify = JSON.parse(JSON.stringify(result[0].body));
-      var blokzmeta = JSON.parse((result[0].json_metadata));
-      console.log(blokify);
-      console.log("blokzmeta: " + blokzmeta);
-
-      console.log(blokzmeta.blokz);
-
-      var bitff = JSON.parse(JSON.stringify(blokzmeta));
-
-      console.log(bitff);
-      document.getElementById("name").value = bitff.name;
-      document.getElementById("nameLabel").style.display = "none";
-      document.getElementById("article").value = bitff.article;
-      document.getElementById("articleLabel").style.display = "none";
-      document.getElementById("usertitle").value = bitff.usertitle;
-      document.getElementById("usertitleLabel").style.display = "none";
-      document.getElementById("birthyear").value = bitff.birthyear;
-      document.getElementById("birthyearLabel").style.display = "none";
-      document.getElementById("sign").value = bitff.sign;
-      document.getElementById("signLabel").style.display = "none";
-      document.getElementById("location").value = bitff.location;
-      document.getElementById("locationLabel").style.display = "none";
-      document.getElementById("gender").value = bitff.gender;
-      document.getElementById("genderLabel").style.display = "none";
-      document.getElementById("interests").value = bitff.interests;
-      document.getElementById("interestsLabel").style.display = "none";
-      document.getElementById("favorites").value = bitff.favorites;
-      document.getElementById("favoritesLabel").style.display = "none";
-      document.getElementById("favsite").value = bitff.favsite;
-      document.getElementById("favsiteLabel").style.display = "none";
-
-    } else {
-      reject(err);
-    }
 
 
-  });
-
-}
-
-
-function updateProfile() {
-
-  // build profile data
-  var data = "<img src='https://blokz.github.io/images/logo512.png'><br />A blokz profile, please click <a href='https://blokz.github.io/profile/?hive=" + document.getElementById('steemagent').value + "' target='_blank'>blokz.github.io/profile/?hive=" + document.getElementById('steemagent').value + "</a> to view.";
-  var article = document.getElementById('article').value;
-  var name = document.getElementById('name').value;
-  var favsite = document.getElementById('favsite').value;
-  var usertitle = document.getElementById('usertitle').value;
-  var birthyear = document.getElementById('birthyear').value;
-  var sign = document.getElementById('sign').value;
-  var gender = document.getElementById('gender').value;
-  var location = document.getElementById('location').value;
-  var interests = document.getElementById('interests').value;
-  var favorites = document.getElementById('favorites').value;
-  console.log("proof: " + favsite + article + name + usertitle + birthyear + sign + gender + location + interests + favorites);
-
-  // profile build finished
-
-
-
-
-
-  hive.broadcast.comment(
-    document.getElementById('postingKey').value,
-    '', //author
-    'blokzprofile', //firsttag
-    document.getElementById('steemagent').value,
-    'blokzprofile', //permlink
-    'My Blokz Profile',
-    data,
-    // json meta
-    {
-      tags: ['blokz'],
-      app: 'blokz',
-      article: article,
-      name: name,
-      favsite: favsite,
-      usertitle: usertitle,
-      birthyear: birthyear,
-      sign: sign,
-      gender: gender,
-      location: location,
-      interests: interests,
-      favorites: favorites
-    },
-    function (err, result) {
-      if (err)
-        alert('failure ' + err);
-      else
-        alert('Profile Updated');
-    }
-  );
-}
-
-
-
-
+// MAIN BODY OF DISPLAYING A PROFILE
 window.onload = function loading() {
-
-  // EXECUTE FOR STEEM
-
-
+  //check if user is set
   if (typeof user !== 'undefined') {
 
-
-    hive.api.getAccounts([user], function (err, result) {
-      console.log(err, result);
-      profdata = JSON.parse(result[0].json_metadata);
-      console.log(profdata);
-      console.log(profdata.profile.cover_image);
-      console.log("TEST :" + profdata.profile.profile_image);
+    // gets posting_json_metadata for generic profile data for user
+    hive.api.call('database_api.find_accounts', { accounts: [user] }, (err, res) => {
+      posting_json = JSON.parse(JSON.stringify(res.accounts[0].posting_json_metadata));
+      console.log("posting_json: " + posting_json );
       var x = document.createElement("IMG");
-      document.getElementById("profimg").src = "https://steemitimages.com/u/" + user + "/avatar"; //profdata.profile.profile_image;
-      document.getElementById("profilecard").style.backgroundImage = "url('https://steemitimages.com/0x0/" + profdata.profile.cover_image + "')";
+       //profdata.profile.profile_image;
 
+      document.getElementById("profimg").src = JSON.parse(posting_json).profile.profile_image;
+  
+       //profdata.profile.cover_image;
+      document.getElementById("coverimage").style.backgroundImage = "url('" +JSON.parse(posting_json).profile.cover_image+"')";
     });
 
     // get recent posts
@@ -172,24 +63,19 @@ window.onload = function loading() {
       console.log(recent1);
       var recent1date = recent1.created.slice(0, 10);
       document.getElementById("recent1").innerHTML = "1. <a href='http://peakd.com/@" + user + "/" + recent1.permlink + "' target='_blank'>" + recent1.title + "</a> <br /><span>&nbsp;</span>Posted on " + recent1date + "<br />";
-
       var recent2 = JSON.parse(JSON.stringify(result[1]));
-      console.log(recent2.permlink);
-
+      console.log(recent2);
       var recent2date = recent2.created.slice(0, 10);
       document.getElementById("recent2").innerHTML = "2. <a href='http://peakd.com/@" + user + "/" + recent2.permlink + "' target='_blank'>" + recent2.title + "</a> <br /><span>&nbsp;</span>Posted on " + recent2date + "<br />";
-
       var recent3 = JSON.parse(JSON.stringify(result[2]));
-      console.log(recent3.permlink);
+      console.log(recent3);
       var recent3date = recent3.created.slice(0, 10);
-
       document.getElementById("recent3").innerHTML = "3. <a href='http://peakd.com/@" + user + "/" + recent3.permlink + "' target='_blank'>" + recent3.title + "</a> <br /><span>&nbsp;</span>Posted on " + recent3date + "<br />";
       document.getElementById("recentM").innerHTML = "<br /><div>View More on <a href='http://peakd.com/@" + user + "/' target='_blank'>peakd.com/@" + user + "</a></div><hr />";
-
     });
 
+    document.getElementById("hiveuser").innerHTML = "<a href='http://peakd.com/@" + user + "' target='_blank'>@" + user + "</a>";
 
-    document.getElementById("steemagent").innerHTML = "<a href='http://peakd.com/@" + user + "' target='_blank'>@" + user + "</a>";
     hive.api.getDiscussionsByAuthorBeforeDate(user, 'blokzprofile', now, 1, (err, result) => {
 
       if (result.length >= 1) {
@@ -251,7 +137,6 @@ window.onload = function loading() {
           // TEST TEST
 
 
-          //  document.getElementById("profimg").src = "https://steemitimages.com/u/" + user + "/avatar"; //profdata.profile.profile_image;
           // CURRENT TODO: FRIEND IMAGE
           console.log("CAUGHT: " + entryy);
 
@@ -292,7 +177,7 @@ window.onload = function loading() {
           var imageParent = document.getElementById(para.id);
           //image.id = ff; 
           image.className = "avatar";
-          image.src = "https://steemitimages.com/u/" + entryy + "/avatar";            // image.src = "IMAGE URL/PATH"
+          image.src = "https://images.hive.blog/u/" + entryy + "/avatar";            // image.src = "IMAGE URL/PATH"
 
 
           imageParent.appendChild(image);
@@ -348,17 +233,60 @@ window.onload = function loading() {
         // steem js bits
       } else {
 
-        console.log("user does not exist! or something went wrong")
-        // document.getElementById("profilecardmini").style.display = "none";
-        document.getElementById("strongLeft").style.display = "none";
 
+        // LOAD GENERIC posting_json_metadata for non blokz/profile user
+        console.log("user does not exist! or something went wrong")
+      // document.getElementById("strongLeft").style.display = "none";
+       // document.getElementById("strongFriends").style.display = "none";
+      // document.getElementById("strongWebsite").style.display = "none";
+
+
+      hive.api.call('database_api.find_accounts', { accounts: [user] }, (err, res) => {
+        posting_json = JSON.parse(JSON.stringify(res.accounts[0].posting_json_metadata));
+        console.log("posting_json: " + posting_json );
+        var x = document.createElement("IMG");
+         //profdata.profile.profile_image;
+        document.getElementById("profimg").src = JSON.parse(posting_json).profile.profile_image;
+         //profdata.profile.cover_image;
+        document.getElementById("coverimage").style.backgroundImage = "url('" +JSON.parse(posting_json).profile.cover_image+"')";
+      
+        if (JSON.parse(posting_json).profile.website !== undefined) {
+          
+          document.getElementById("favsite").innerHTML = "<a href='" + JSON.parse(posting_json).profile.website + "' target='_blank'>" + JSON.parse(posting_json).profile.website + "</a>";
+        } else {
+
+          document.getElementById("strongWebsite").style.display = "none";
+        }
+
+        if (JSON.parse(posting_json).profile.location !== undefined) {
+        document.getElementById("location").innerHTML = JSON.parse(posting_json).profile.location;
+      } else {
+        document.getElementById("strongLocation").style.display = "none";
+      }
+        document.getElementById("usertitle").innerHTML = JSON.parse(posting_json).profile.about;
+        document.getElementById("name").innerHTML = JSON.parse(posting_json).profile.name;
+
+
+        document.getElementById("strongAge").style.display = "none";
+        document.getElementById("strongSign").style.display = "none";
+        document.getElementById("strongGender").style.display = "none";
         document.getElementById("strongFriends").style.display = "none";
-        document.getElementById("strongWebsite").style.display = "none";
+        document.getElementById("strongAbout").style.display = "none";
+        document.getElementById("strongFollow").style.display = "none";
+
+
+        //console.log("Location: " +JSON.parse(posting_json).profile.location);
+
+      });
+      //document.getElementById("locationLabel").style.display = "none";
+
+      
       }
 
 
     });
   } else {
+    // initial page load without user set
     console.log("IT WORKS!! user not set");
     document.getElementById("gridd").style.display = "none";
   }
@@ -370,10 +298,95 @@ window.onload = function loading() {
 
 
 
+// search username on profile_update
+function hiveuserUp() {
+  console.log("TRIGGERED!!!");
+  let hiveuser = document.getElementById("hiveuser").value;
+  console.log(hiveuser);
+  hive.api.getDiscussionsByAuthorBeforeDate(hiveuser, 'blokzprofile', now, 1, (err, result) => {
+    // populate data
+    if (result) {
+      console.log("results are in:");
+      console.log(result);
+      var blokify = JSON.parse(JSON.stringify(result[0].body));
+      var blokzmeta = JSON.parse((result[0].json_metadata));
+      console.log(blokify);
+      console.log("blokzmeta: " + blokzmeta);
+      console.log(blokzmeta.blokz);
+      var bitff = JSON.parse(JSON.stringify(blokzmeta));
+      console.log(bitff);
+      document.getElementById("name").value = bitff.name;
+      document.getElementById("nameLabel").style.display = "none";
+      document.getElementById("article").value = bitff.article;
+      document.getElementById("articleLabel").style.display = "none";
+      document.getElementById("usertitle").value = bitff.usertitle;
+      document.getElementById("usertitleLabel").style.display = "none";
+      document.getElementById("birthyear").value = bitff.birthyear;
+      document.getElementById("birthyearLabel").style.display = "none";
+      document.getElementById("sign").value = bitff.sign;
+      document.getElementById("signLabel").style.display = "none";
+      document.getElementById("location").value = bitff.location;
+      document.getElementById("locationLabel").style.display = "none";
+      document.getElementById("gender").value = bitff.gender;
+      document.getElementById("genderLabel").style.display = "none";
+      document.getElementById("interests").value = bitff.interests;
+      document.getElementById("interestsLabel").style.display = "none";
+      document.getElementById("favorites").value = bitff.favorites;
+      document.getElementById("favoritesLabel").style.display = "none";
+      document.getElementById("favsite").value = bitff.favsite;
+      document.getElementById("favsiteLabel").style.display = "none";
+    } else {
+      reject(err);
+    }
+  });
+}
 
 
+function updateProfile() {
+  var data = "<img src='https://blokz.github.io/images/logo512.png'><br />A blokz profile, please click <a href='https://blokz.github.io/profile/?hive=" + document.getElementById('hiveuser').value + "' target='_blank'>blokz.github.io/profile/?hive=" + document.getElementById('hiveuser').value + "</a> to view.";
+  var article = document.getElementById('article').value;
+  var name = document.getElementById('name').value;
+  var favsite = document.getElementById('favsite').value;
+  var usertitle = document.getElementById('usertitle').value;
+  var birthyear = document.getElementById('birthyear').value;
+  var sign = document.getElementById('sign').value;
+  var gender = document.getElementById('gender').value;
+  var location = document.getElementById('location').value;
+  var interests = document.getElementById('interests').value;
+  var favorites = document.getElementById('favorites').value;
+  console.log("proof: " + favsite + article + name + usertitle + birthyear + sign + gender + location + interests + favorites);
 
-
+  hive.broadcast.comment(
+    document.getElementById('postingKey').value,
+    '', //author
+    'blokzprofile', //firsttag
+    document.getElementById('hiveuser').value,
+    'blokzprofile', //permlink
+    'My Blokz Profile',
+    data,
+    // json meta
+    {
+      tags: ['blokz'],
+      app: 'blokz',
+      article: article,
+      name: name,
+      favsite: favsite,
+      usertitle: usertitle,
+      birthyear: birthyear,
+      sign: sign,
+      gender: gender,
+      location: location,
+      interests: interests,
+      favorites: favorites
+    },
+    function (err, result) {
+      if (err)
+        alert('failure ' + err);
+      else
+        alert('Profile Updated');
+    }
+  );
+}
 
 
 
