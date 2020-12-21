4ab8b67aa4354e4a19ee8b6a9c2195a61b33df64
snahn ip <are.zee.dee@gmail.com>
2020-12-20 19:00:09 -0800

    working on pwa stuff

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -3,14 +3,24 @@ self.addEventListener('install', function(e) {
 		caches.open('cacheTitle').then(function(cache) {
 			return cache.addAll([
 				'index.html',
+				'profile_update/index.html',
+				'playground/index.html',
 				'manifest.json',
 				'sw.js',
 				'images/favicon.png',
 				'images/logo512.png',
 				'images/logo192.png',
+				'images/back.png',
+				'images/peakd.png',
+				'images/hivestats.png',
 				'js/blokz.js',
-				'js/steem.js',
+				'js/aes256.min.js',
+				'js/mdl-chips-input.js',
+				'js/sanitize-html.js',
+				'js/wordlist.js',
 				'css/styles.css',
+				'css/mdl-chips-input.css',
+				'css/mdl-chips-input.css.map'
 			]);
 		})
 	);
